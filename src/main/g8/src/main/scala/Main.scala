599be0f5fafe9957f7585ac4ef564db43f75c488
Lens <lensgolda@gmail.com>
2025-12-02 16:13:40 +0200

    fix Main.scala - escape $ sign

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -23,8 +23,8 @@ object BasicConsumer extends ZIOAppDefault {
                       .withClientId(config.kafka.clientId)
               saslSettings = Map(
                 "sasl.mechanism" -> config.kafka.sasl.mechanism,
-                // "sasl.jaas.config" -> s"""org.apache.kafka.common.security.plain.PlainLoginModule required username="${config.kafka.sasl.username}" password="${config.kafka.sasl.password}";""",
-                "sasl.jaas.config" -> s"""org.apache.kafka.common.security.scram.ScramLoginModule required username="${config.kafka.sasl.username}" password="${config.kafka.sasl.password}";""",
+                // "sasl.jaas.config" -> s"""org.apache.kafka.common.security.plain.PlainLoginModule required username="\${config.kafka.sasl.username}" password="\${config.kafka.sasl.password}";""",
+                "sasl.jaas.config" -> s"""org.apache.kafka.common.security.scram.ScramLoginModule required username="\${config.kafka.sasl.username}" password="\${config.kafka.sasl.password}";""",
                 "security.protocol" -> config.kafka.sasl.securityProtocol,
                 "enable.auto.commit" -> "false"
                 // "log_level" -> "DEBUG"
@@ -40,26 +40,26 @@ object BasicConsumer extends ZIOAppDefault {
     val runConsumer: ZIO[AppConfig & Consumer, Throwable, Unit] = for {
         _ <- ZIO.logInfo(s">>> Starting App >>>")
         kafkaConfig <- ZIO.serviceWith[AppConfig](_.kafka)
-        _ <- ZIO.logInfo(s">>> KafkaConfig: $kafkaConfig")
+        _ <- ZIO.logInfo(s">>> KafkaConfig: \$kafkaConfig")
         subscription <- subscriptionService
         consumer <- ZIO.service[Consumer]
         _ <- consumer
             .plainStream(subscription, Serde.string, Serde.string)
             .take(3)
             // .tap(record =>
-            //     ZIO.logInfo(s"Received: ${record.key} -> ${record.value}")
+            //     ZIO.logInfo(s"Received: \${record.key} -> \${record.value}")
             // )
             // .mapZIO { record =>
-            //     ZIO.logInfo(s"Received: ${record.key} -> ${record.value}")
+            //     ZIO.logInfo(s"Received: \${record.key} -> \${record.value}")
             //         *> record.offset.commit
             // }
             // .runDrain
             .runForeach(record =>
-                ZIO.logInfo(s"Received: ${record.key} -> ${record.value}")
+                ZIO.logInfo(s"Received: \${record.key} -> \${record.value}")
                 // *> record.offset.commit
             )
             .catchAll(err =>
-                ZIO.logError(s"Stream drain error $err")
+                ZIO.logError(s"Stream drain error \$err")
                     *> ZIO.fail(err).orDie
             )
     } yield ()
